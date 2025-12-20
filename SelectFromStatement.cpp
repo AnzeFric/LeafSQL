0b89dab7fcb870ee83596d076d5a50295294cee7
AnzeFric <anzeifric@gmail.com>
2025-12-20 20:20:04 +0100

    Added SelectFromStatement Class

---
+++ SelectFromStatement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anzef on 20. 12. 2025.
+//
+
+#include "SelectFromStatement.h"
---
+++ SelectFromStatement.h
@@ -0,0 +1,13 @@
+//
+// Created by anzef on 20. 12. 2025.
+//
+
+#ifndef LEAFSQL_SELECTFROMSTATEMENT_H
+#define LEAFSQL_SELECTFROMSTATEMENT_H
+
+
+class SelectFromStatement {
+};
+
+
+#endif //LEAFSQL_SELECTFROMSTATEMENT_H
