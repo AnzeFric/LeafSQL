adfec5bbb9c798b3d4308e4d00fe1325ab5a5325
AnzeFric <anzeifric@gmail.com>
2025-12-20 20:24:34 +0100

    Updated main and added testing logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "QueryParser/QueryParser.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const std::string query = "SELECT * FROM table;";
+
+    QueryParser queryParser = QueryParser(query);
+    std::cout << "Parsing:" << std::endl;
+    queryParser.parseQuery();
+    std::cout << "Printing:" << std::endl;
+    queryParser.print();
+
     return 0;
 }
