1b75ea32970332c186f7bc33d2e5f17c11a6bbf1
AnzeFric <anzeifric@gmail.com>
2025-12-21 13:19:18 +0100

    Added KeywordToken

---
+++ Token/KeywordToken/KeywordToken.cpp
@@ -0,0 +1,32 @@
+//
+// Created by anzef on 21. 12. 2025.
+//
+
+#include "KeywordToken.h"
+
+KeywordType stringToKeywordType(const std::string& value) {
+    static const std::unordered_map<std::string, KeywordType> keywordMap = {
+        {"SELECT", KeywordType::SELECT},
+        {"INSERT", KeywordType::INSERT},
+        {"DELETE", KeywordType::DELETE},
+        {"UPDATE", KeywordType::UPDATE}
+    };
+
+    if (const auto it = keywordMap.find(value); it != keywordMap.end()) {
+        return it->second;
+    }
+
+    throw std::runtime_error("Unknown keyword: " + value);
+}
+
+KeywordToken::KeywordToken(const TokenType type, const std::string &value, const std::string& keywordStr): Token(type, value) {
+    this->keywordType = stringToKeywordType(keywordStr);
+};
+
+KeywordToken::KeywordToken(const Token& token, const std::string& keywordStr) : Token(token) {
+    this->keywordType = stringToKeywordType(keywordStr);
+};
+
+KeywordType KeywordToken::getKeywordType() const {
+    return this->keywordType;
+};
---
+++ Token/KeywordToken/KeywordToken.h
@@ -0,0 +1,23 @@
+//
+// Created by anzef on 21. 12. 2025.
+//
+
+#ifndef LEAFSQL_KEYWORDTOKEN_H
+#define LEAFSQL_KEYWORDTOKEN_H
+
+#include <unordered_map>
+#include <stdexcept>
+#include <string>
+#include "../Token.h"
+#include "../Types/KeywordType.h"
+
+class KeywordToken : public Token {
+private:
+    KeywordType keywordType;
+public:
+    KeywordToken(TokenType type, const std::string &value, const std::string& keywordStr);
+    KeywordToken(const Token& token, const std::string& keywordStr);
+    KeywordType getKeywordType() const;
+};
+
+#endif //LEAFSQL_KEYWORDTOKEN_H
