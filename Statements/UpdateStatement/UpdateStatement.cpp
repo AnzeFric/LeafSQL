4c3f7a0fccd9113c65f71f4ae11fd862abb96502
AnzeFric <anzeifric@gmail.com>
2025-12-30 13:33:10 +0100

    Added UpdateStatement

---
+++ Statements/UpdateStatement/UpdateStatement.cpp
@@ -0,0 +1,21 @@
+//
+// Created by anzef on 30. 12. 2025.
+//
+
+#include "UpdateStatement.h"
+
+UpdateStatement::UpdateStatement() {};
+
+UpdateStatement::UpdateStatement(std::string table, std::vector<std::string> columns, std::vector<std::string> values): table(std::move(table)), columns(std::move(columns)), values(std::move(values)) {};
+
+void UpdateStatement::setTable(const std::string& table) {
+    this->table = std::move(table);
+};
+
+void UpdateStatement::setColumns(const std::vector<std::string>& columns) {
+    this->columns = columns;
+};
+
+void UpdateStatement::setValues(const std::vector<std::string>& values) {
+    this->values = values;
+};
---
+++ Statements/UpdateStatement/UpdateStatement.h
@@ -0,0 +1,24 @@
+//
+// Created by anzef on 30. 12. 2025.
+//
+
+#ifndef LEAFSQL_UPDATESTATEMENT_H
+#define LEAFSQL_UPDATESTATEMENT_H
+
+#include <string>
+#include <vector>
+
+class UpdateStatement {
+private:
+    std::string table;
+    std::vector<std::string> columns;
+    std::vector<std::string> values;
+public:
+    UpdateStatement();
+    UpdateStatement(std::string table, std::vector<std::string> columns, std::vector<std::string> values);
+    void setTable(const std::string& table);
+    void setColumns(const std::vector<std::string>& columns);
+    void setValues(const std::vector<std::string>& values);
+};
+
+#endif //LEAFSQL_UPDATESTATEMENT_H
