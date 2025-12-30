b32f16890ffa105a69e0bcfd0e5dc98c13dc9c0a
AnzeFric <anzeifric@gmail.com>
2025-12-30 20:13:35 +0100

    Updated main with new test cases

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include "QueryParser/QueryParser.h"
 
 int main() {
-    const std::string query = "SELECT * FROM table;";
+    const std::string selectQuery = "SELECT * FROM table;";
+    const std::string createDBQuery = "CREATE DATABASE LeafDB;";
+    const std::string createTableQuery = "CREATE TABLE LeafTable";
 
-    QueryParser queryParser = QueryParser(query);
+    QueryParser queryParser = QueryParser(createDBQuery);
     queryParser.parseQuery();
     queryParser.print();
 
