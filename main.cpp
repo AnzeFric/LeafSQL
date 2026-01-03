260664a29851b71786f4d85d6fc2abb2380e744f
AnzeFric <anzeifric@gmail.com>
2026-01-03 13:29:46 +0100

    Updated main, added test case for USE DB;

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 int main() {
     const std::string selectQuery = "SELECT * FROM table;";
     const std::string createDBQuery = "CREATE DATABASE LeafDB;";
+    const std::string useDBQuery = "USE LeafDB;";
     const std::string createTableQuery = "CREATE TABLE LeafTable";
 
-    QueryParser queryParser = QueryParser(createDBQuery);
+    QueryParser queryParser = QueryParser(useDBQuery);
+    queryParser.parseQuery();
+    queryParser.print();
+
+    queryParser.setQuery(createTableQuery);
     queryParser.parseQuery();
     queryParser.print();
 
