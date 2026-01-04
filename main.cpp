b1fc98083984b68335d696a3401f2e97ee50e575
AnzeFric <anzeifric@gmail.com>
2026-01-04 17:10:08 +0100

    Updated main, added INSERT INTO test case

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@ int main() {
     const std::string createDBQuery = "CREATE DATABASE LeafDB;";
     const std::string useDBQuery = "USE LeafDB;";
     const std::string createTableQuery = "CREATE TABLE users (id INT PRIMARY KEY, username TEXT NOT NULL UNIQUE, email TEXT NOT NULL UNIQUE, age INT32, created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, is_active BOOLEAN DEFAULT TRUE);";
+    const std::string insertQuery = "INSERT INTO users (id, name, email, created_at) VALUES (1, Alice, alice@example.com, NOW());";
 
     QueryParser queryParser = QueryParser(useDBQuery);
     queryParser.parseQuery();
 
     queryParser.setQuery(createTableQuery);
+    queryParser.parseQuery();
+
+    queryParser.setQuery(insertQuery);
     queryParser.parseQuery();
 
     return 0;
