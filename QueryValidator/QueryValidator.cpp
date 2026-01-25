ace5e1a317ede38dc8432fa726750a50594ed062
AnzeFric <anzeifric@gmail.com>
2026-01-25 19:48:32 +0100

    Updated QueryValidator, added function to check valid input by type

--- QueryValidator/QueryValidator.cpp
+++ QueryValidator/QueryValidator.cpp
@@ -3,6 +3,38 @@
 //
 
 #include "QueryValidator.h"
+#include "DataType/DataType.h"
+
+void strToUpper(std::string& str) {
+    for (auto & c: str) {
+        c = static_cast<char>(toupper(c));
+    }
+}
+
+bool isValidValue(std::string& value, DataType::Type type) {
+    switch(type) {
+        case DataType::INT:
+            try {
+                std::stoi(value);
+                return true;
+            } catch (...) {
+                return false;
+            }
+
+        case DataType::TEXT:
+            return true;
+
+        case DataType::BOOLEAN:
+            strToUpper(value);
+            return (value == "TRUE" || value == "FALSE" || value == "1" || value == "0");
+
+        case DataType::TIMESTAMP:
+            return true; // TODO: Make format check
+
+        default:
+            return false; // Invalid type
+    }
+}
 
 void QueryValidator::validateSelectQuery(SelectFromStatement selectFromStatement) {}
 
