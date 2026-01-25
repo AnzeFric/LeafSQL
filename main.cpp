0df698511abbe6a68d2037908e6101801f63a59c
AnzeFric <anzeifric@gmail.com>
2026-01-25 19:44:23 +0100

    Updated test case for creating table

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     const std::string selectQuery = "SELECT * FROM table;";
     const std::string createDBQuery = "CREATE DATABASE LeafDB;";
     const std::string useDBQuery = "USE LeafDB;";
-    const std::string createTableQuery = "CREATE TABLE users (id INT PRIMARY, username TEXT UNIQUE, email TEXT UNIQUE, age NULL INT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, is_active BOOLEAN DEFAULT TRUE);";
+    const std::string createTableQuery = "CREATE TABLE users (id INT PRIMARY AUTO, username TEXT UNIQUE, email TEXT UNIQUE, age NULL INT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, is_active BOOLEAN DEFAULT TRUE);";
     const std::string insertQuery = "INSERT INTO users (id, username, email, created_at, is_active) VALUES (1, Alice, alice@example.com, NOW(), true);";
 
     QueryParser queryParser = QueryParser(useDBQuery);
