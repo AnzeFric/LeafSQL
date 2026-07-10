ef7613563edbef9d7a776af8a967b46b6395f075
AnzeFric <anzeifric@gmail.com>
2026-07-10 09:53:05 +0200

    Shared - Globals. Created a global variable for the current active db and store it's name. To prevent tunneling and deep parameter passing even when not necessary.

---
+++ Shared/Globals.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Uporabnik on 10. 07. 2026.
+//
+
+#include "Globals.h"
+
+std::string g_activeDbName;
---
+++ Shared/Globals.h
@@ -0,0 +1,12 @@
+//
+// Created by Uporabnik on 10. 07. 2026.
+//
+
+#ifndef LEAFSQL_GLOBALS_H
+#define LEAFSQL_GLOBALS_H
+
+#include <string>
+
+extern std::string g_activeDbName;
+
+#endif //LEAFSQL_GLOBALS_H
