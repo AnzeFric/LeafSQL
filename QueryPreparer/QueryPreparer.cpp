225e4cd74104e6103fcdc5dddd9249ea246efc8d
AnzeFric <anzeifric@gmail.com>
2026-07-17 11:56:11 +0200

    QueryPreparer - prepareDeleteQuery. Added logic to prepare the array with indexes to delete based on WHERE statement

--- QueryPreparer/QueryPreparer.cpp
+++ QueryPreparer/QueryPreparer.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <stdexcept>
 #include <numeric>
+#include <sstream>
 
 std::vector<std::string> getDefinitionFilePaths(const std::string& tableName) {
     const std::string dirPathStr = "data/" + g_activeDbName + "/" + tableName + "/";
@@ -94,11 +95,79 @@ void QueryPreparer::prepareInsertQuery(InsertIntoStatement insertIntoStatement) 
 // TODO: Make prepare update query
 void QueryPreparer::prepareUpdateQuery(UpdateStatement updateStatement) {}
 
-// TODO: Prepare the query based on WHERE clause
 void QueryPreparer::prepareDeleteQuery(DeleteFromStatement deleteFromStatement) {
-    deleteFromStatement.setDeleteRowIndexes({0});
-
     try {
+        std::vector<int> deleteRowIndexes;
+
+        const std::string tableName = deleteFromStatement.getTable();
+        std::vector<Condition> conditions = deleteFromStatement.getConditions();
+
+        const std::vector<std::string> filePaths = getDefinitionFilePaths(tableName);
+
+        const std::vector<std::string> tableColumns = Utils::getFileRowsCSV(filePaths[0]);
+
+        // Check that all table columns exist
+        for (auto& condition : conditions) {
+            const auto it = std::ranges::find(tableColumns, condition.column);
+
+            if (it == tableColumns.end()) {
+                throw std::runtime_error("Column not found: " + condition.column);
+            }
+
+            // Column exists. Get its index
+            condition.columnIndex = std::distance(tableColumns.begin(), it);
+        }
+
+        const std::string& dataFilePath = filePaths[2];
+
+        std::ifstream in(dataFilePath);
+
+        std::string line;
+        int lineIndex = 0;
+
+        while (std::getline(in, line)) {
+
+            std::vector<std::string> fields;
+
+            std::stringstream ss(line);
+            std::string field;
+
+            while (std::getline(ss, field, ',')) {
+                if (!field.empty() && field.front() == ' ') {
+                    field.erase(0, 1);
+                }
+                fields.push_back(field);
+            }
+
+            bool conditionRes;
+            LogicalOperator prevOperator = END;
+
+            for (const auto& condition : conditions) {
+
+                bool conditionValue = condition.value == fields[condition.columnIndex];
+
+                if (prevOperator == END) {
+                    conditionRes = conditionValue;
+                } else if (prevOperator == AND) {
+                    conditionRes = conditionRes && conditionValue;
+                } else {
+                    conditionRes = conditionRes || conditionValue;
+                }
+
+                prevOperator = condition.logicalOperator;
+            }
+
+            if (conditionRes) {
+                deleteRowIndexes.push_back(lineIndex);
+            }
+            ++lineIndex;
+        }
+
+        in.close();
+
+        deleteFromStatement.setDeleteRowIndexes(deleteRowIndexes);
+
+
         QueryValidator::validateDeleteQuery(deleteFromStatement);
     } catch (const std::exception& e) {
         throw std::runtime_error("DELETE FROM TABLE '" + deleteFromStatement.getTable() + "': " + e.what());
